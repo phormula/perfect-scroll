7f84ae68fa8a7b3f8f9dde3e71f89b9a2fe2c4f9
Evans Teiko <evanstteiko@gmail.com>
2023-01-04 22:33:09 +0100

    initial commit

---
+++ projects/p-scrollbar/src/public-api.ts
@@ -0,0 +1,9 @@
+/*
+ * Public API Surface of perfect-scrollbar
+ */
+
+export * from './lib/perfect-scrollbar.component';
+export * from './lib/perfect-scrollbar-force-native-scroll.directive';
+export * from './lib/perfect-scrollbar.directive';
+export * from './lib/perfect-scrollbar.interfaces';
+export * from './lib/perfect-scrollbar.module';
